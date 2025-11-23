ee46384b3702d60b2b02668d400913dee60922e5
Rajix24 <rajix2100@gmail.com>
2025-11-23 20:24:22 +0100

    change of the logique I use arr to store data in eatch zone the localSotorage is working the part that affich the employer are working the filtrage and also the modal u can put eatch element in the zone but they are some problems the 2Eme time it showe in the both zone I don't know why but It need the working

--- script.js
+++ script.js
@@ -1,332 +1,165 @@
-let idGeneratorCounter = 1;
-let employeList = [];
+const empunsigeZone = document.getElementById("workers")
+const modalList = document.getElementById("cardslist")
+//zones
+const zone = document.querySelectorAll(".zone")
+const zone1 = document.getElementById("borde1")
+const zone2 = document.getElementById("borde2")
+const zone3 = document.getElementById("borde3")
+const zone4 = document.getElementById("borde4")
+const zone5 = document.getElementById("borde5")
+const zone6 = document.getElementById("borde6")
+//variables
+let listEmp = [];
+let zoneMeeting = [];
+let zoneResiption = [];
+let zoneServer = [];
+let zoneScuirty = [];
+let zoneEmp = [];
+let zoneArchiff = [];
+let empCardId = 1;
 
-let mettingRomme = [];
-let receptionRomme = [];
-let srveurRomme = [];
-let scurityRomme =[];
-let employeRomme = [];
-let archiveRomme = [];
-let modalList = [];
-
-window.addEventListener("load", () =>{
-    employeList =  JSON.parse(localStorage.getItem('employelist')) || []
-    randerWorker(workersPlaceHolder, employeList);
-})
-employeList =  JSON.parse(localStorage.getItem('employelist')) || []
-    
-   
-
-const workersPlaceHolder = document.getElementById("workers");
-const metting = document.getElementById("borde1");
-const cardModalList = document.getElementById("cardslist");
-const employee = document.querySelectorAll(".worker")
-//=======================================================
-const workerRome = document.getElementById("borde5")
-
-
-
-modalAddBtn.addEventListener("click" , () => {
-    const form = document.forms["modalForm"]
-    
-console.log(form.modalName.value)//the value are there
-
-    const employer = {
-        id : idGeneratorCounter,
-        name : form.modalName.value,
-        role : form.modalRole.value,
-        email : form.modalEmail.value,
-        tele : form.modalTele.value,
-        exprience:[]
-        }
-    // console.log(employer)//the value are there
-
-    employeList.push(employer)
-    console.log(employeList)
-
-
-    setToLocalStorage('employelist',employeList);
-    
-    randerWorker(workersPlaceHolder, [employer]);
-    idGeneratorCounter++;
+window.addEventListener('load', () => {
+    listEmp = JSON.parse(localStorage.getItem('employer')) || []
+    randerList(empunsigeZone, listEmp)
 })
 
-//that function is rerander the madal
-document.querySelectorAll(".rerander").forEach(element => {
-     element.addEventListener("click", ()=> {
-         document.getElementById("exp").innerHTML = ""
-    }) 
-});
-//a daymanic imput for Exp
-document.getElementById("addExp").addEventListener("click", () => {
-    let card = `
-                                                <form class="d-flex my-5 gap-2 flex-column" id="dd" name="">
-                                                <h4>exp</h4>
-                                                    <lable>Company</lable> 
-                                                    <input type="text" name="conpany" id="companyDInput" class="form-control test">
-                                                    <lable>Role</lable> 
-                                                    <input type="text" name="role" id ="roleDInput" class="form-control test2">
-                                                    <lable>Duree</lable> 
-                                                    <input type="number" name="duree" id ="dureeDInput" class="dureeDInput form-control test3">
-                                                </form>
+document.getElementById("modalAddBtn").addEventListener("click", ()=>{
+    addEmp()
+
+})
+function addEmp() {
+    const form = document.forms["modalForm"];
+    let emp = {
+        id: empCardId,
+        name :form.modalName.value,
+        role :form.modalRole.value,
+        email :form.modalEmail.value,
+        exp: []
+    }
+    const formEXP = document.querySelectorAll('.formExp')
+    getExpdata(formEXP, emp.exp)
+    listEmp.push(emp)
+    empCardId++;
+    empunsigeZone.innerHTML = "",
+    randerList(empunsigeZone, listEmp)
+    setToLocalStorage("employer", listEmp)
+    getItmesFromLocalStorange("employer")
+}
+
+function setToLocalStorage(keyname, Array){
+    if (localStorage.getItem("employer")) {
+        listEmp = JSON.parse(localStorage.getItem('employer'))
+    }
+    localStorage.setItem(keyname, JSON.stringify(Array))
+}
+function getItmesFromLocalStorange(keyname) {
+    const dataString = localStorage.getItem(keyname)
+    listEmp = JSON.parse(dataString)
+}
+
+document.getElementById("addExp").addEventListener("click", expAddInHTML)
+function expAddInHTML (){
+    const expAddInHTMLContext = `
+         <form name="formExp" id="formExp" class="formExp">
+            <input type="text" class="form-control" name="company">
+            <input type="text" class="form-control" name="role">
+            <input type="number" class="form-control" name="duree">
+         </form>
+    
     `
-    document.getElementById("exp").insertAdjacentHTML('beforeend',card) 
+    document.getElementById("exp").insertAdjacentHTML("beforeend", expAddInHTMLContext)
+}
+
+function getExpdata(form, arr){
+  form.forEach(element => {
+      let obj ={
+            company: element.company.value,
+            role: element.role.value,
+            duree: element.duree.value
+        }
+        arr.push(obj)
+    });
+    console.log(arr)
+}
+
+function randerList(position, array) {
+    array.forEach(element => {
+        position.innerHTML+= `
+                        <div class="worker bg-dfault p-2 d-flex gap-3 align-items-center justify-content-sm-evenly" id="${element.id}">
+                            <div class="worker-img">
+                                <img src="https://avatar.iran.liara.run/public" alt="photo-image " class="border rounded-circle " id="image" width="60px" height="60px">
+                            </div>
+                            <div class="worker-info d-flex flex-column pt-3 " >
+                                <h6 class="fs-4 w-100 border"  >${element.name}</h6>
+                                <p class="worker-position Role" name ="para">${element.role}</p>
+                            </div>
+                            <button class="btn btn-danger border btnZones">X</button>
+                        </div>
+                            `
+    });
+}
+
+zone1.addEventListener("click", ()=>{
+    modalList.innerHTML ="" 
+    document.getElementById("thatisModal").style.display="block"
+    let empFilter = listEmp.filter(element => element.role == "Manager" || element.role == "IT")
+    randerList(modalList, empFilter)
+    addToZone(zone1, empFilter)
+})
+zone2.addEventListener("click", ()=>{
+    modalList.innerHTML ="" 
+    document.getElementById("thatisModal").style.display="block"
+    let empFilter = listEmp.filter(element => element.role == "Receptionists" || element.role == "IT"|| element.role == "Manager")
+    randerList(modalList, empFilter)
+    addToZone(zone2, empFilter)
+})
+zone3.addEventListener("click", ()=>{
+    modalList.innerHTML ="" 
+    document.getElementById("thatisModal").style.display="block"
+    let empFilter = listEmp.filter(element => element.role == "Manager" || element.role == "IT"|| element.role == "Cleaning")
+    randerList(modalList, empFilter)
+    addToZone(zone3, empFilter)
+})
+zone4.addEventListener("click", ()=>{
+    modalList.innerHTML ="" 
+    document.getElementById("thatisModal").style.display="block"
+    let empFilter = listEmp.filter(element => element.role == "Manager" || element.role == "IT"|| element.role == "security")
+    randerList(modalList, empFilter)
+    addToZone(zone4, empFilter)
+})
+zone5.addEventListener("click", ()=>{
+    modalList.innerHTML ="" 
+    document.getElementById("thatisModal").style.display="block"
+    randerList(modalList, listEmp)
+    addToZone(zone5, listEmp)
+})
+zone6.addEventListener("click", ()=>{
+    modalList.innerHTML ="" 
+    document.getElementById("thatisModal").style.display="block"
+    let empFilter = listEmp.filter(element => element.role !== "Cleaning")
+    randerList(modalList, empFilter)
+    addToZone(zone6, listEmp)
 })
 
-//function that get items to local storage NB I will add some of the condition if it empty or if someone doublecat 
-function setToLocalStorage(keyName, obj) {
-    if(localStorage.getItem('employelist')){
-        employeList = JSON.parse(localStorage.getItem('employelist'))
-    }
-    localStorage.setItem(keyName, JSON.stringify(obj));
+
+document.getElementById("colseBar").addEventListener("click", close)
+function close(){
+        document.getElementById("thatisModal").style.display="none"
+
 }
-//function that getting the items from local storage
-function getFromLocalStorage() {
-    const dataString = localStorage.getItem(counter)
-    const data = JSON.parse(dataString);
-    return data;
-}
-
-function randerWorker(position, DataList) {
-    DataList.forEach(element => {
-    position.innerHTML +=`
-                    <div class="worker bg-dfault p-2 d-flex gap-3 align-items-center justify-content-sm-evenly" id="${idGeneratorCounter}">
-                        <div class="worker-img">
-                            <img   src="https://avatar.iran.liara.run/public" alt="photo-image " class="border rounded-circle " id="image" width="60px" height="60px">
-                        </div>
-                        <div class="worker-info d-flex flex-column pt-3 " >
-                            <h6 class="fs-4 w-100 border"  >${element.name}</h6>
-                            <p class="worker-position Role" name ="para">${element.role}</p>
-                        </div>
-                        <button class="btn btn-danger border" id="testing" >X</button>
-                    </div> 
-    `   
-    modalList = []
-    });
-    return position;
-}
-// remove()
-// function remove () {
-//     document.getElementById("").addEventListener("click", () =>{
-//         console.log("test test mmmmmm")
-// })
-// }
-
-
-
-
-// ==================================== function for eatch borde ==========
-
-document.getElementById("btn1").addEventListener("click", meetingRome)
-function meetingRome(){
-    document.getElementById('thatisModal').style.display ="block";
-    employeList.forEach(element => {
-        if (element.role == "Manager") {
-            let index = employeList.indexOf(element)
-            modalList.push(element)
-            employeList.splice(index, 1)
-            employeList.pop()
-            randerWorker(cardModalList, modalList)
-        }
-        cardModalList.addEventListener("click", ToMeeting)
-    });
-
-
-
-
-
-
-    //fuction that rander one element 
-    // how can I wirthe to one element 
-
-
-
-    //  const cardWorker = document.querySelectorAll('.worker')
-    //  const place = document.getElementById("cardslist")
-    //  const border = document.querySelector(".borde1")
-    //  document.getElementById('thatisModal').style.display ="block"
-    //  if(border.childElementCount > 1){
-    //      BB.outerHTML = ``;
-    //      document.getElementById('thatisModal').style.display ="none"
-    //      returnAllworkerToWorkerPlace()
-     
-    //  cardWorker.forEach(element => {
-    //  if( element.querySelector('.Role').innerHTML == "Manager" || element.querySelector('.Role').innerHTML == "Cleaning"){
-    //      place.appendChild(element)
-    //     } 
-    //     }
-    //  document.getElementById("cardslist").addEventListener('click', () =>{
-    //      cardWorker.forEach(element => {
-    //          element.addEventListener("click", ()=>{
-    //              border.appendChild(element)
-    //          })
-    //      });      
-    //  })
-    //  )}
-}
-
-document.getElementById("btn5").addEventListener("click", workersRomme)
-function workersRomme () {
-    document.getElementById('thatisModal').style.display ="block";
-    employeList.forEach(element => {
-            modalList.push(element)
-            let index = employeList.indexOf(element)
-            randerWorker(cardModalList, modalList)
-        cardModalList.addEventListener("click", toWorker)
-    });
-}
-function archifRomme (){
-    document.getElementById('thatisModal').style.display ="block"//that is show the modal
-    const cardWorker = document.querySelectorAll(".worker");
-    const place = document.getElementById("cardslist")
-    cardWorker.forEach(element => {   //list of the array
-        if(element.querySelector('#WorkeRole').innerHTML !== "Cleaning"){
-            place.appendChild(element)
-        }
-    })
-    document.getElementById("cardslist").addEventListener('click', addToBord)
-    
-    function addToBord () {
-        const cardWorker = document.querySelectorAll(".worker");
-        const border = document.querySelector(".borde6")
-        cardWorker.forEach(element => {
-            element.addEventListener("click", ()=>{
-                border.appendChild(element)
-            })
-        });      
-        if(border.childElementCount > 2){
-            BB.outerHTML = ``;
-            returnAllworkerToWorkerPlace()
-        }
-    }
-}
-function servuerRomme(){
-    document.getElementById('thatisModal').style.display ="block"//that is show the modal
-    const cardWorker = document.querySelectorAll(".worker");
-    const place = document.getElementById("cardslist")
-    cardWorker.forEach(element => {   //list of the array
-        if(element.querySelector('#WorkeRole').innerHTML == "IT" ||element.querySelector('#WorkeRole').innerHTML == "Manager"){
-            place.appendChild(element)
-        }
-    })
-    document.getElementById("cardslist").addEventListener('click', addToBord)
-    function addToBord () {
-        const BB = document.getElementById("btn3");
-        const cardWorker = document.querySelectorAll(".worker");
-        const border = document.querySelector(".borde3")
-        cardWorker.forEach(element => {
-            element.addEventListener("click", ()=>{
-                border.appendChild(element)
-            })
-        });      
-        if(border.childElementCount > 2){
-            BB.outerHTML = ``;
-            returnAllworkerToWorkerPlace()
-        }
-    }
-}
-function secrityRomme() {
-    document.getElementById('thatisModal').style.display ="block"//that is show the modal
-    const cardWorker = document.querySelectorAll(".worker");
-    const place = document.getElementById("cardslist")
-    cardWorker.forEach(element => {   //list of the array
-        if(element.querySelector('#WorkeRole').innerHTML == "secrity" ||element.querySelector('#WorkeRole').innerHTML == "Manager"){
-            place.appendChild(element)
-        }
-    })
-    document.getElementById("cardslist").addEventListener('click', addToBord)
-    function addToBord () {
-        const BB = document.getElementById("btn4");
-        const cardWorker = document.querySelectorAll(".worker");
-        const border = document.querySelector(".borde4")
-        cardWorker.forEach(element => {
-        element.addEventListener("click", ()=>{
-            border.appendChild(element)
+function addToZone(zoneNum, EmpFilter) {
+        modalList.addEventListener("click", (e)=>{
+            let targetedElement = e.target
+            let findIdOfEmp = EmpFilter.filter(element => element.id == targetedElement.id)
+            delete listEmp[targetedElement.id -1]
+            empunsigeZone.innerHTML =" "
+            randerList(empunsigeZone,listEmp)
+            randerList(zoneNum, findIdOfEmp)
+            close()
         })
-    });      
-    if(border.childElementCount > 2){
-        BB.outerHTML = ``;
-        returnAllworkerToWorkerPlace()
-    }
 }
-}
-function Receptionists() {
-    document.getElementById('thatisModal').style.display ="block"//that is show the modal
-    const cardWorker = document.querySelectorAll(".worker");
-    const place = document.getElementById("cardslist")
-    
-    cardWorker.forEach(element => {   //list of the array
-        if(element.querySelector('#WorkeRole').innerHTML == "Receptionists" ||element.querySelector('#WorkeRole').innerHTML == "Manager"){
-            place.appendChild(element)
-        }
-    })
-    document.getElementById("cardslist").addEventListener('click', addToBord)
-    function addToBord () {
-        const BB = document.getElementById("btn2");
-        const cardWorker = document.querySelectorAll(".worker");
-        const border = document.querySelector(".borde2")
-        cardWorker.forEach(element => {
-            element.addEventListener("click", ()=>{
-                border.appendChild(element)
-            })
-        });      
-        if(border.childElementCount > 3){
-            BB.outerHTML = ``;
-            returnAllworkerToWorkerPlace()
-        }
-    }
-    
-}
-
-function returnAllworkerToWorkerPlace (){   
-    document.getElementById('thatisModal').style.display ="none"
-    const cardsss = document.getElementById('cardslist')
-    cardsss.querySelectorAll(".worker").forEach(element => {
-        document.getElementById("workers").appendChild(element)
-    });
-}
-
-
-document.getElementById("colseBar").addEventListener("click", cleos)
-function cleos () {
-    document.getElementById('thatisModal').style.display ="none";
-    // modalList.length = 0;
-}
-//mettingRomme
-function ToMeeting(event) {
-    const clickedElement = event.target;
-    modalList.forEach(element => {
-        if (clickedElement.id == element.id) {
-            mettingRomme.push(element)
-            randerOneEmploye(metting, element)
-        }
-    });
-    
-}
-function toWorker (event) {
-    const clickedElement = event.target;
-    modalList.forEach(element => {
-        if (clickedElement.id == element.id) {
-            mettingRomme.push(element)
-            randerOneEmploye(workerRome, element)
-        }
-    });
-}
-
-
-
-
-function randerOneEmploye(position, obj) {
-    position.innerHTML +=`
-    <div class="worker bg-dfault p-2 d-flex gap-3 align-items-center justify-content-sm-evenly" id="A-${idGeneratorCounter}">
-    <div class="worker-img">
-    <img   src="https://avatar.iran.liara.run/public" alt="photo-image " class="border rounded-circle " id="image" width="60px" height="60px">
-    </div>
-    <div class="worker-info d-flex flex-column pt-3 " >
-    <h6 class="fs-4 w-100 border"  >${obj.name}</h6>
-    <p class="worker-position Role" name ="para">${obj.role}</p>
-    </div>
-    <button class="btn btn-danger border" id="testing" >X</button>
-    </div> 
-    `   
+removeToSideBar()
+function removeToSideBar () {
+    btnRemove = document.querySelectorAll(".btnZones")
+    console.log(btnRemove)
 }
